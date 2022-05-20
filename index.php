0eee16d83a0ef94e0470d923d984d9a4b6ff3496
CalebMUC <muchiricaleb05@gmail.com>
2022-05-20 08:23:03 +0300

    adding new files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alvet Agrovet & Animals Feeds  </title>
+</head>
+<body>
+    <h1>Alvet Agrovet & Animal Feeds CO.</h1>
+    <img src="./images/logo.jpeg" alt="logo image">
+    <ul>
+        <li><a href="index.php">index</a></li>
+        <li><a href="products.php">products</a></li>
+        <li><a href="Mycart.php">Mycart</a></li>
+        <li><a href="About.php">About</a></li>
+</ul>
+
+<img src="./images/wheat.jpeg" width="260" alt="bakground image">
+<br>
+<br>
+
+
+<p>copyright &copy;</p>
+</body>
+</html>
